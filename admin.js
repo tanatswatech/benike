0d9510cb64dbdc803835d1d23bf27edc6cb96f49
tanatswatech <tanatswatech@gmail.com>
2026-06-15 14:41:20 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,109 @@
+loadStats();
 
+function uploadExcel(){
+
+const file =
+document.getElementById("excelFile").files[0];
+
+if(!file){
+alert("Choose Excel file");
+return;
+}
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+
+const data =
+new Uint8Array(e.target.result);
+
+const workbook =
+XLSX.read(data,{type:"array"});
+
+const sheet =
+workbook.Sheets[
+workbook.SheetNames[0]
+];
+
+const products =
+XLSX.utils.sheet_to_json(sheet);
+
+localStorage.setItem(
+"products",
+JSON.stringify(products)
+);
+
+alert(
+products.length +
+" products imported"
+);
+
+loadStats();
+showProducts();
+
+};
+
+reader.readAsArrayBuffer(file);
+
+}
+
+function loadStats(){
+
+const products =
+JSON.parse(
+localStorage.getItem("products")
+) || [];
+
+document.getElementById(
+"totalProducts"
+).innerText = products.length;
+
+const categories =
+new Set(
+products.map(
+p=>p.Category
+)
+);
+
+document.getElementById(
+"totalCategories"
+).innerText =
+categories.size;
+
+}
+
+function showProducts(){
+
+const products =
+JSON.parse(
+localStorage.getItem("products")
+) || [];
+
+const container =
+document.getElementById(
+"previewContainer"
+);
+
+container.innerHTML="";
+
+products.forEach(product=>{
+
+container.innerHTML +=
+
+`
+<div class="product-preview">
+
+<h3>${product.Name}</h3>
+
+<p>$${product.Price}</p>
+
+<p>${product.Category}</p>
+
+</div>
+`;
+
+});
+
+}
+
+showProducts();
